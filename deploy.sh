532d98d42bea8e38bad29bcb97591715a8785484
root <shivap16@gmail.com>
2019-08-07 14:21:38 -0700

    updating shell script to build deployment

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,5 @@
+rm -rf deployment || true
+
 docker run -v $PWD:/tmp tibbar/vortex:v1 -template /tmp/templates -output /tmp/deployment -varpath /tmp/environments/default.yaml
 
 kubectl create ns nifi
